a234774f179f4c19b7c3dcc6e7e902e4e1f49693
Heyden Aldana <heyden.aldana@unitec.edu>
2025-01-04 02:41:57 -0600

    Ya se pueden efectuar operaciones de suma, resta, multiplicacion y division, validando casos de indeterminacion

--- calculadoraFracciones.asm
+++ calculadoraFracciones.asm
@@ -13,13 +13,15 @@
 	sms_preguntarDen2: .asciiz "\n- Por favor, digite el segundo  denominador: "
 	sms_newLine: .asciiz "\n"
 	sms_opcionInvalida: .asciiz "\n- AVISO: Debe digitar un número dentro de las opciones disponibles\n\n"
-
-	sms_mas: .asciiz "  +  "
-	sms_menos: .asciiz "  -  "
-	sms_mul: .asciiz "  *  "
-	sms_div: .asciiz "  /  "
+	sms_ErrorDenominador: .asciiz "\n- ERROR: No se puede realizar la operacion por indeterminacion\n"
+	sms_ErrorDivision: .asciiz "\n- ERROR: No se puede realizar la division porque el resultado\n  es una fraccion con denominador cero, lo cual es INDETERMINACION\n\n"
+	
+	sms_mas: .asciiz "   +   "
+	sms_menos: .asciiz "   -   "
+	sms_mul: .asciiz "   *   "
+	sms_div: .asciiz "   /   "
 	sms_simboloFraccion: .asciiz " / "
-	sms_igual: .asciiz "  =  "
+	sms_igual: .asciiz "   =   "
 	
 	num1: .word 0
 	den1: .word 1
@@ -63,8 +65,14 @@
 		li $v0, 5
 		syscall
 		move $t4, $v0
-		
-		
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		# Relizar operacion segun opcion elegida y se muestra (se hace por interno)
+		beq $t0, 1, suma
+		beq $t0, 2, resta
+		beq $t0, 3, multiplicacion
+		beq $t0, 4, division
 		# Repetir el bucle main
 		li $v0, 4
 		la $a0, sms_newLine
@@ -75,8 +83,263 @@
 			li $v0, 10
 			syscall
 		
+	# --- OPERACIONES MATEMATICAS
+	# Operacion de suma (manejo de varios casos)
+	suma:
+		beq $t2, 0, msgErrorDenominador
+		beq $t4, 0, msgErrorDenominador
+		# Suma por metodo XD
+		mul $t5, $t1, $t4
+		mul $t6, $t2, $t3
+		mul $t7, $t2, $t4
+		add $t6, $t6, $t5
+		# --- Mostrar suma de fracciones
+		# Imprimir primera fraccion
+		li $v0, 1
+		add $a0, $zero, $t1
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t2
+		syscall
+		# Imprimir simbolo correspondiente
+		li $v0, 4
+		la $a0, sms_mas
+		syscall
+		# Imprimir segunda fraccion
+		li $v0, 1
+		add $a0, $zero, $t3
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t4
+		syscall
+		# Imprimir simbolo igual
+		li $v0, 4
+		la $a0, sms_igual
+		syscall
+		# Imprimir fraccion resultante (hacer validaciones)
+		li $v0, 1
+		add $a0, $zero, $t6
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t7
+		syscall
+		# Volver al main
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		j main
+		
+	# Operacion de resta (manejo de varios casos similar a la suma)
+	resta:
+		beq $t2, 0, msgErrorDenominador
+		beq $t4, 0, msgErrorDenominador
+		# Resta por metodo XD
+		mul $t5, $t1, $t4
+		mul $t6, $t2, $t3
+		mul $t7, $t2, $t4
+		sub $t6, $t6, $t5
+		# --- Mostrar resta de fracciones
+		# Imprimir primera fraccion
+		li $v0, 1
+		add $a0, $zero, $t1
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t2
+		syscall
+		# Imprimir simbolo correspondiente
+		li $v0, 4
+		la $a0, sms_menos
+		syscall
+		# Imprimir segunda fraccion
+		li $v0, 1
+		add $a0, $zero, $t3
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t4
+		syscall
+		# Imprimir simbolo igual
+		li $v0, 4
+		la $a0, sms_igual
+		syscall
+		# Imprimir fraccion resultante (hacer validaciones)
+		li $v0, 1
+		add $a0, $zero, $t6
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t7
+		syscall
+		# Volver al main
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		j main
+		
+	# Operacion de multiplicación
+	multiplicacion:
+		beq $t2, 0, msgErrorDenominador
+		beq $t4, 0, msgErrorDenominador
+		# Suma por metodo XD
+		mul $t6, $t1, $t3
+		mul $t7, $t3, $t4
+		# --- Mostrar multiplicacion de fracciones
+		# Imprimir primera fraccion
+		li $v0, 1
+		add $a0, $zero, $t1
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t2
+		syscall
+		# Imprimir simbolo correspondiente
+		li $v0, 4
+		la $a0, sms_mul
+		syscall
+		# Imprimir segunda fraccion
+		li $v0, 1
+		add $a0, $zero, $t3
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t4
+		syscall
+		# Imprimir simbolo igual
+		li $v0, 4
+		la $a0, sms_igual
+		syscall
+		# Imprimir fraccion resultante (hacer validaciones)
+		li $v0, 1
+		add $a0, $zero, $t6
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t7
+		syscall
+		# Volver al main
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		j main
+		
+	# Operacion de division (cuidado con el numerador cero)
+	division:
+		# Para dividir, se multiplica cruzado en X
+		mul $t6, $t1, $t4
+		mul $t7, $t2, $t3
+		# Se evalua despues del resultado, no antes
+		beq $t7, 0, msgErrorDivision
+		# --- Mostrar division de fracciones
+		# Imprimir primera fraccion
+		li $v0, 1
+		add $a0, $zero, $t1
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t2
+		syscall
+		# Imprimir simbolo correspondiente
+		li $v0, 4
+		la $a0, sms_div
+		syscall
+		# Imprimir segunda fraccion
+		li $v0, 1
+		add $a0, $zero, $t3
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t4
+		syscall
+		# Imprimir simbolo igual
+		li $v0, 4
+		la $a0, sms_igual
+		syscall
+		# Imprimir fraccion resultante (hacer validaciones)
+		li $v0, 1
+		add $a0, $zero, $t6
+		syscall
+		li $v0, 4
+		la $a0, sms_simboloFraccion
+		syscall
+		li $v0, 1
+		add $a0, $zero, $t7
+		syscall
+		# Volver al main
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		li $v0, 4
+		la $a0, sms_newLine
+		syscall
+		j main
+		
+		
+	# --- MANEJO DE ERRORES Y VALIDACIONES MATEMATICAS
+	# Se muestra mensaje si alguno de los denominadores es cero (para division se maneja diferente)
+	msgErrorDenominador:
+		jal limpiarRegistros
+		li $v0, 4
+		la $a0, sms_ErrorDenominador
+		syscall
+		j main
+		
+	# Se muestra mensaje si algun numerador es cero (SOLO DIVISION)
+	msgErrorDivision:
+		jal limpiarRegistros
+		li $v0, 4
+		la $a0, sms_ErrorDivision
+		syscall
+		j main
+	
+	# Mostrar mensaje de error si el usuario no ingresa una opcion valida
 	mostrarMensajeOpcionInvalida:
 		li $v0, 4
 		la $a0, sms_opcionInvalida
 		syscall 
 		j main
+		
+	# Restablece los valores de los registros a cero
+	limpiarRegistros:
+		addi $t1, $zero, 0 
+		addi $t2, $zero, 0 
+		addi $t3, $zero, 0 
+		addi $t4, $zero, 0 
+		addi $t5, $zero, 0 
+		addi $t6, $zero, 0 
+		addi $t7, $zero, 0 
+		jr $ra
